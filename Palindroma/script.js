5d5c2030bcc9e56de072b5fc4cc616e1f93c407c
Carlo Costa <carlo.costa92@gmail.com>
2025-02-28 14:43:54 +0100

    feat: base structure js

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -4,3 +4,17 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
 */
+
+// richiesta parola all'utente
+
+
+
+/* 
+creo funzione che prenda i caratteri della parola inserita e li inserisca in un array,
+utilizzando un for, in ordine inverso e con un if verifico che la parola inserita dall'utente sia uguale a quella 
+riscritta inversa
+*/
+
+
+
+//invoco la funzione
